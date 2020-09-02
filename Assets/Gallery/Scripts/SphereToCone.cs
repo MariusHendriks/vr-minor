9e9c02e43abafd8a15e4f797dd4542e6e0529de7
Mark <mark.hendriks9999@gmail.com>
2020-09-02 16:33:22 +0200

    Added the basics and the task of week 1, wednesday

---
+++ Assets/Gallery/Scripts/SphereToCone.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereToCone : MonoBehaviour
+{
+    public float height = 2f;
+    public float radius = 1f;
+    public float xRotation = 8f;
+    public float yRotation = 8f;
+    private float x;
+    private float y;
+    private bool collided = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Vector3> points = new List<Vector3>();
+        points.Add(new Vector3(0, 0, 0));
+        var steps = 30;
+
+        List<int> triangles = new List<int>();
+
+        for (int i = 0; i < steps; i++)
+        {
+            points.Add(new Vector3(Mathf.Cos(i * 2 * Mathf.PI / steps) * radius, 0, Mathf.Sin(i * 2 * Mathf.PI / steps) * radius));
+        }
+
+        points.Add(new Vector3(0, height, 0));
+
+        for (int i = 0; i < steps; i++)
+        {
+            triangles.Add(0);
+            triangles.Add(i + 1);
+            if (i == steps - 1)
+            {
+                triangles.Add(1);
+            }
+            else
+            {
+                triangles.Add(i + 2);
+            }
+        }
+
+        for (int i = 0; i < steps; i++)
+        {
+            triangles.Add(i + 1);
+            triangles.Add(31);
+            if (i == steps - 1)
+            {
+                triangles.Add(1);
+            }
+            else
+            {
+                triangles.Add(i + 2);
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = points.ToArray();
+        mesh.triangles = triangles.ToArray();
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!collided)
+        {
+            x += xRotation * Time.deltaTime; // Needed to be done or else eulerangel would stop at 90 degrees
+            y += yRotation * Time.deltaTime;
+            transform.eulerAngles = new Vector3(x, y, 0);
+        }
+
+
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        collided = true;
+
+    }
+}
---
+++ Assets/Gallery/Scripts/SphereToCube.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SphereToCube : MonoBehaviour
+{
+    public float xRotation = 8f;
+    public float yRotation = 8f;
+    private float x;
+    private float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] verticles =
+        {
+            new Vector3(-0.5f ,-0.5f, 0.5f), //0
+            new Vector3(-0.5f ,0.5f, 0.5f), //1
+            new Vector3(0.5f ,-0.5f, 0.5f), //2
+            new Vector3(0.5f ,0.5f, 0.5f), //3
+            new Vector3(-0.5f ,-0.5f, -0.5f), //4
+            new Vector3(-0.5f ,0.5f, -0.5f), //5
+            new Vector3(0.5f ,-0.5f, -0.5f), //6
+            new Vector3(0.5f ,0.5f, -0.5f), //7
+        };
+        int[] triangles = { 1, 0, 2, 1, 2, 3, 0, 5, 4, 0, 1, 5, 0, 4, 2, 4, 6, 2, 1, 3, 5, 5, 3, 7, 4, 7, 6, 4, 5, 7, 7, 3, 6, 6, 3, 2 };
+
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = verticles;
+        mesh.triangles = triangles;
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += xRotation * Time.deltaTime; // Needed to be done or else eulerangel would stop at 90 degrees
+        y += yRotation * Time.deltaTime;
+        transform.eulerAngles = new Vector3(x, y, 0);
+    }
+}
+
